77b32af22d140b69fecc5fbc63eaa5bdbc2b18fa
ThomasAbrial <thomas.abrial@laposte.net>
2023-02-21 16:01:21 +0100

    IDAO + DOA Chat

--- src/main/java/dao/DAOChat.java
+++ src/main/java/dao/DAOChat.java
@@ -2,32 +2,58 @@ package dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import context.Singleton;
 import model.Chat;
 
 public class DAOChat implements IDAOChat{
 
 	@Override
 	public Chat findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		Chat chat = em.find(Chat.class,id);
+		em.close();
+		return chat;
 	}
 
 	@Override
 	public List<Chat> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		List<Chat> chats = em.createQuery("from Chat").getResultList();
+		em.close();
+		return chats;
 	}
 
 	@Override
-	public Chat save(Chat o) {
-		// TODO Auto-generated method stub
-		return null;
+	public Chat save(Chat chat) {
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		em.getTransaction().begin();
+		chat = em.merge(chat);
+		em.getTransaction().commit();
+		em.close();
+		return chat;
 	}
 
 	@Override
 	public void delete(Integer id) {
-		// TODO Auto-generated method stub
-		
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		Chat chat = em.find(Chat.class,id);
+		em.getTransaction().begin();
+		em.remove(chat);
+		em.getTransaction().commit();
+		em.close();	
+	}
+
+	@Override
+	public List<Chat> findAllByClient(Integer idClient) {
+		EntityManager em = Singleton.getInstance().getEmf().createEntityManager();
+		Query ps = em.createQuery("from Chat c where c.client.id=:idClient");
+		ps.setParameter("idClient", idClient);
+		List<Chat> chats = ps.getResultList();
+		em.close();
+		return chats;
 	}
 
 }
--- src/main/java/dao/IDAOChat.java
+++ src/main/java/dao/IDAOChat.java
@@ -1,7 +1,10 @@
 package dao;
 
+import java.util.List;
+
 import model.Chat;
 
 public interface IDAOChat extends IDAO<Chat,Integer> {
-
+	
+	public List<Chat> findAllByClient(Integer idClient);
 }
