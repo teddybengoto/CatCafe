684dd824c053cde49cb542642b429ac8773bd66e
Jordan Rolland <jordan.rolland78@gmail.com>
2023-02-21 16:04:31 +0100

    DAO Adoption/Garde

--- src/main/java/dao/IDAOAdoption.java
+++ src/main/java/dao/IDAOAdoption.java
@@ -1,8 +1,14 @@
 package dao;
 
+import java.util.List;
+
 import model.Adoption;
 import model.Chat;
+import model.Reservation;
 
 public interface IDAOAdoption extends IDAO<Adoption,Integer>{
+	
+	public List<Adoption> findAllByClient(Integer idClient);
+	public List<Adoption> findAllByChat(Integer idChat);
 
 }
