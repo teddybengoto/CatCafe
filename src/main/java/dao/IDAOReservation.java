e645a43932bb0513c12412f12982361d72b1bd98
Alicia <alicia.monfort0@orange.fr>
2023-02-21 15:52:16 +0100

    IDAO + DAO reservation

--- src/main/java/dao/IDAOReservation.java
+++ src/main/java/dao/IDAOReservation.java
@@ -1,8 +1,11 @@
 package dao;
 
+import java.util.List;
+
 import model.Chat;
 import model.Reservation;
 
 public interface IDAOReservation extends IDAO<Reservation,Integer> {
-
+	
+	public List<Reservation> findAllByClient(Integer idClient);
 }
