a1c0897f9e275b46937fa00eec1c544136c752ec
teddybengoto <teddybengoto@gmail.com>
2023-02-22 10:10:38 +0100

    Add dao Comptes

--- src/main/java/dao/IDAOCompte.java
+++ src/main/java/dao/IDAOCompte.java
@@ -1,8 +1,15 @@
 package dao;
 
-import model.Chat;
+import java.util.List;
+
+import model.Admin;
+import model.Client;
 import model.Compte;
 
 public interface IDAOCompte extends IDAO<Compte,Integer>{
 
+    public Compte findByLoginAndPassword(String login,String password);
+	
+	public List<Admin> findAllAdmin();
+	public List<Client> findAllClient();
 }
