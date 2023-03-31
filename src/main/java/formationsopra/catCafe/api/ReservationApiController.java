a310e568e9eba0b151d9935acab3a4a873c187a7
teddybengoto <teddybengoto@gmail.com>
2023-03-31 15:05:01 +0200

    Ras

--- src/main/java/formationsopra/catCafe/api/ReservationApiController.java
+++ src/main/java/formationsopra/catCafe/api/ReservationApiController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
