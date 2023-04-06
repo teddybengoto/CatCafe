b899069773bf6a4419b7050d4b635eaf8412e71d
teddybengoto <teddybengoto@gmail.com>
2023-04-06 16:39:36 +0200

    add upload files

--- src/main/java/formationsopra/catCafe/api/ChatApiController.java
+++ src/main/java/formationsopra/catCafe/api/ChatApiController.java
@@ -1,10 +1,21 @@
 package formationsopra.catCafe.api;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
+//import org.apache.commons.io.FileUtils;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.apache.tomcat.util.http.parser.MediaTypeCache;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +25,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
@@ -26,119 +40,166 @@ import formationsopra.catCafe.model.Client;
 import formationsopra.catCafe.request.ChatRequest;
 import jakarta.validation.Valid;
 
-
+import org.springframework.web.multipart.MultipartFile;
+import formationsopra.catCafe.utils.saveImage;
+import io.micrometer.common.util.StringUtils;
 
 @RestController
 @RequestMapping("/api/chat")
 @CrossOrigin("*")
 public class ChatApiController {
-	
+
 	@Autowired
 	private IDAOChat daoChat;
-	
-	//----------FindAll----------
+    	private String imageLocation = "../../catcafefront/src/assets/img/api/chat";
+    	private String imageFrontLocation = "../../../assets/img/api/chat/";
+
+
+	// ----------FindAll----------
 	@GetMapping
 	@JsonView(Views.Chat.class)
 	public List<Chat> findAll() {
 		return this.daoChat.findAll();
 	}
-	
+
 	@GetMapping("/adoptable")
 	@JsonView(Views.Chat.class)
 	public List<Chat> findAllAdoptable() {
 		return this.daoChat.findAllAdoptable();
 	}
-	
+
 	@GetMapping("/permanent")
 	@JsonView(Views.Chat.class)
 	public List<Chat> findAllPermanent() {
 		return this.daoChat.findAllPermanent();
 	}
-	
-	//----------FindById----------
+
+	// ----------FindById----------
 	@GetMapping("/{id}")
 	@JsonView(Views.Chat.class)
-	public Chat findById(@PathVariable int id){
+	public Chat findById(@PathVariable int id) {
 		return this.daoChat.findById(id).orElseThrow(ChatNotFoundException::new);
-		
+
 	}
-	
+
 	//
 	@GetMapping("/by-client-id/{clientId}")
 	@JsonView(Views.Chat.class)
-	public List<Chat> findAllByClientId(@PathVariable int clientId){
+	public List<Chat> findAllByClientId(@PathVariable int clientId) {
 		return this.daoChat.findAllByClientId(clientId);
 	}
-	
-	
-	//----------ADD----------
+
+	// ----------ADD----------
 	@PostMapping
 	@JsonView(Views.Chat.class)
 	public Chat add(@RequestBody @Valid ChatRequest chatRequest, BindingResult result) {
 		if (result.hasErrors()) {
 			throw new ChatBadRequestException();
 		}
-		
+
 		Chat chat = new Chat();
 		BeanUtils.copyProperties(chatRequest, chat);
-		if(chatRequest.getClientId() != null) {
+		if (chatRequest.getClientId() != null) {
 			Client client = new Client();
 			client.setId(chatRequest.getClientId());
 			chat.setClient(client);
 		}
-		
+
 		else {
 			chat.setClient(null);
 		}
-		
+
 		return this.daoChat.save(chat);
-		
+
 	}
-	
-	//----------EDIT----------
+
+	// ----------EDIT----------
 	@PutMapping("/{id}")
 	@JsonView(Views.Chat.class)
 	public Chat edit(@PathVariable int id, @RequestBody @Valid ChatRequest chatRequest, BindingResult result) {
 		if (result.hasErrors()) {
 			throw new ChatBadRequestException();
 		}
-		
+
 		Chat chat = this.daoChat.findById(id).orElseThrow(ChatNotFoundException::new);
-		
+
 		BeanUtils.copyProperties(chatRequest, chat);
 		System.out.println(chatRequest.getClientId());
-		
-		if(chatRequest.getClientId() != null) {
+
+		if (chatRequest.getClientId() != null) {
 			Client client = new Client();
 			client.setId(chatRequest.getClientId());
 			chat.setClient(client);
 		}
-		
+
 		else {
 			chat.setClient(null);
 		}
-		
-		
+
 		return this.daoChat.save(chat);
 	}
-	
-	//----------Delete----------
+
+	// ----------Delete----------
 	@DeleteMapping("/{id}")
 	public boolean deleteById(@PathVariable int id) {
 		try {
 			this.daoChat.findById(id).orElseThrow(ChatNotFoundException::new);
-			
+
 			this.daoChat.deleteById(id);
-			
+
 			return true;
 		}
-		
+
 		catch (Exception e) {
 			return false;
 		}
-	
+
 	}
-	
-	
-	
+
+	// Add Image
+
+	@PutMapping("/image/{id}")
+	@JsonView(Views.Compte.class)
+	public Chat editImage(@PathVariable int id, @RequestParam("file") MultipartFile multipartFile) throws IOException {
+
+		String fileName = multipartFile.getOriginalFilename();
+		String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+		saveImage.saveFile(imageLocation, (id+"."+extension), multipartFile);
+
+		Chat c = this.daoChat.findById(id).orElseThrow(ChatNotFoundException::new);
+		c.setImage(imageFrontLocation + id +"."+ extension);
+
+		return this.daoChat.save(c);
+	}
+
+	// return image Dont work for now
+
+	/*@GetMapping("/image")
+	public @ResponseBody byte[] getImage() throws IOException {
+
+
+		/*InputStream in = getClass().getResourceAsStream(imageLocation+"1.png");	
+
+		return IOUtils.toByteArray(in);/
+		//Pat file = root.resolve(filename);
+		//Resource resource = new UrlResource(file.toUri());
+		//File outputfile = new File(imageLocation+"1.png");
+
+
+        //try {
+		//	InputStreamResource resource = new InputStreamResource(new FileInputStream(outputfile));
+
+
+       // } catch (IOException e) {
+         //   e.printStackTrace();
+       // }
+	//return resource;
+	}*/
+
+	/*@ResponseBody
+	@RequestMapping(value = "/photo2", method = RequestMethod.GET, produces = MediaTypeCache.IMAGE_JPEG_VALUE)
+	public byte[] testphoto() throws IOException {
+		InputStream in = servletContext.getResourceAsStream("/images/no_image.jpg");
+		return IOUtils.toByteArray(in);
+	}*/
 }
---
+++ src/main/java/formationsopra/catCafe/utils/saveImage.java
@@ -0,0 +1,34 @@
+package formationsopra.catCafe.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+
+import org.springframework.web.multipart.MultipartFile;
+
+
+public class saveImage {
+
+    private String imageLocation = "../../catcafefront/public/img/chat/";
+
+    public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException {
+        Path uploadPath = Paths.get(uploadDir);
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            System.out.println("filePath: "+filePath);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new IOException("Could not save image file: " + fileName, ioe);
+        }
+    }
+
+}
